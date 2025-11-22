22bab47def2ebd27eefaa116a8adcbb0a73fff41
veeraprathapreddykataru <veeraprathapreddy.kataru@slu.edu>
2025-11-22 20:02:08 +0000

    Full Stack Prints

--- app.js
+++ app.js
@@ -16,9 +16,12 @@ app.use(middleware.cors)
 app.get('/', api.handleRoot)
 app.get('/products', api.listProducts)
 app.get('/products/:id', api.getProduct)
+app.get('/orders', api.listOrders)
+app.get('/orders/', api.createOrder)
+app.put('/orders/:id', api.editOrder);
+app.delete('/orders/:id', api.deleteOrder);
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
-
